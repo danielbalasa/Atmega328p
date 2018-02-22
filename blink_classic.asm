b02b59f4e1ae88cb619162b4af84fc0b71d7dde9
danielbalasa <daniel.balasa@gmail.com>
2018-02-22 01:07:16 +0100

    Update blink_classic.asm

--- blink_classic.asm
+++ blink_classic.asm
@@ -41,10 +41,10 @@
 
 ;---------------------------------------------------------------------------
 RESET:	ldi r16,high(RAMEND)		; Main program start here after reset
-		out SPH,r16					; Set Stack Pointer to top of RAM
+		out SPH,r16		; Set Stack Pointer to top of RAM
 		ldi r16,low(RAMEND)
 		out SPL,r16
-		sei							; Enable interrupts
+		sei			; Enable interrupts
 
 		sbi DDRB, DDB5				; define pin13 as an OUTPUT pin - set pin 5 of the IO register 0x04 to 1
 
@@ -53,18 +53,18 @@ _loop:
 		rcall _delay				; jump to the counting loop
 		cbi PORTB, PORTB5			; turn LED off
 		rcall _delay				; jump to the counting loop again
-		rjmp _loop					; go back at the begining of the main loop
+		rjmp _loop				; go back at the begining of the main loop
  
 _delay:
 		ldi r24, 0x00				; one second delay iteration - load register r24 with 0x00
 		ldi r23, 0xd4				; load reggister r23 with value 0xd4 (212)
 		ldi r22, 0x30				; load reggister r22 with value 0x30 (48)
-_d1:								; delay ~1 second - the counting mechanism
-		subi r24, 1					; substract 1 from r24 -> r24 = r24 - 1 - SET CARRY FLAG IF 0-1
-		sbci r23, 0					; substract only the carry flag from the previous instruction - SET ALSO THE CARRY FLAG 
-		sbci r22, 0					; substract only the carry flag from the previous instruction - SET ALSO THE CARRY FLAG 
-		brcc _d1					; jump to _d1 if there is no carry - basically jump to _d1 as long as r22 did not reach 0
-		ret							; this way the total number of iterations are 255 x 212 x 48 = 2594880 cycles at 16Mhz the CPU time is aprox 1 sec
+_d1:							; delay ~1 second - the counting mechanism
+		subi r24, 1				; substract 1 from r24 -> r24 = r24 - 1 - SET CARRY FLAG IF 0-1
+		sbci r23, 0				; substract only the carry flag from the previous instruction - SET ALSO THE CARRY FLAG 
+		sbci r22, 0				; substract only the carry flag from the previous instruction - SET ALSO THE CARRY FLAG 
+		brcc _d1				; jump to _d1 if there is no carry - basically jump to _d1 as long as r22 did not reach 0
+		ret					; this way the total number of iterations are 255 x 212 x 48 = 2594880 cycles at 16Mhz the CPU time is aprox 1 sec
 
 
 ;----------------------------------------------------------------------------
