a064d6fe85313cbeac07a3fc901e5a2e4f889539
danielbalasa <daniel.balasa@gmail.com>
2020-05-01 14:38:50 +0200

    Update blink.asm

--- blink.asm
+++ blink.asm
@@ -37,7 +37,7 @@
 	; this is the vector interupts table
 	; delete the RETI instructions, uncoment the JMP instructions and implement the routines in the code segment
 	
- 	jmp RESET		; Reset - vector address contain a jump to the routine that executes after reset - main program - this is the first instruction to execute after reset
+ 	jmp _reset		; Reset - vector address contain a jump to the routine that executes after reset - main program - this is the first instruction to execute after reset
 	reti			; jmp INT0		; IRQ0
 	reti 			; jmp INT1		; IRQ1
 	reti 			; jmp PCINT0		; PCINT0
@@ -67,11 +67,36 @@
 
 ;---------------------------------------------------------------------------
 
-RESET:					; Main program start here after reset
+_reset:					; Main program start here after reset
 
 		ldi r16,high(RAMEND)	; initialize the Stack Pointer at the end of the SRAM memory
 		out SPH,r16		; Set Stack Pointer to top of SRAM (the high byte)
 		ldi r16,low(RAMEND)	; RAMEND is declared in <mp328pdef.inc>
 		out SPL,r16		; Set Stack Pointer to top of SRAM (the low byte)
 		sei			; Enable interrupts
+
+		sbi DDRB, DDB5				; define pin13 as an OUTPUT pin - set pin 5 of the IO register 0x04 to 1
+
+_mainloop:
+		sbi 0x03, PORTB5			; turn LED on - set pin 5 of the IO register 0x03 to 1
+		rcall _delay				; jump to the counting loop
+		cbi PORTB, PORTB5			; turn LED off
+		rcall _delay				; jump to the counting loop again
+		rjmp _mainloop				; go back at the begining of the main loop
+ 
+_delay:
+		ldi r24, 0x00				; one second delay iteration - load register r24 with 0x00
+		ldi r23, 0xd4				; load reggister r23 with value 0xd4 (212)
+		ldi r22, 0x30				; load reggister r22 with value 0x30 (48)
+_d1sec:							; delay ~1 second - the counting mechanism
+		subi r24, 1				; substract 1 from r24 -> r24 = r24 - 1 - SET CARRY FLAG IF 0-1
+		sbci r23, 0				; substract only the carry flag from the previous instruction - SET ALSO THE CARRY FLAG 
+		sbci r22, 0				; substract only the carry flag from the previous instruction - SET ALSO THE CARRY FLAG 
+		brcc _d1sec				; jump to _d1 if there is no carry - basically jump to _d1 as long as r22 did not reach 0
+		ret					; this way the total number of iterations are 255 x 212 x 48 = 2594880 cycles at 16Mhz the CPU time is aprox 1 sec
+
+
+;----------------------------------------------------------------------------
+; the rest of the code including the definition of the interrupt routines
+
 
