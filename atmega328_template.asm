811caf332af7d1e5137f2dd93e25ac7d07b03c96
danielbalasa <daniel.balasa@gmail.com>
2020-05-01 12:52:16 +0200

    Update atmega328_template.asm

--- atmega328_template.asm
+++ atmega328_template.asm
@@ -16,13 +16,13 @@
  
  .DSEG						; Data Segment
 	.ORG 0x0100				; place the data in memory at this adderss (in SRAM Data Memory) - only .BYTE directives here - 1 BYTE Wide
-	var1: .BYTE 1			; reserve 1 byte to var1
+	var1: .BYTE 1				; reserve 1 byte to var1
 
 ; ---------- EEPROM SEGMENT ----------
 
  .ESEG						; EEPROM Segment
 	.ORG 0x0000				; place the data in memory at this adderss (in EEPROM Data Memory)  - 1 BYTE Wide
-	 eevar1: .DW 0xAAAA		; initialize 1 word in EEPROM
+	 eevar1: .DW 0xAAAA			; initialize 1 word in EEPROM
 
  ; ---------- CODE SEGMENT ----------
 
@@ -30,12 +30,12 @@
 	.ORG 0x0000				; place the code in memory at this adderss 0x0000 (the begining of FLASH MEMORY) - 1 WORD Wide
 
  	jmp RESET		; Reset - vector address contain a jump to the routine that executes after reset - main program - this is the first instruction to execute after reset
-	reti			; jmp INT0			; IRQ0
-	reti 			; jmp INT1			; IRQ1
+	reti			; jmp INT0		; IRQ0
+	reti 			; jmp INT1		; IRQ1
 	reti 			; jmp PCINT0		; PCINT0
 	reti 			; jmp PCINT1		; PCINT1
 	reti 			; jmp PCINT2		; PCINT2
-	reti 			; jmp WDT			; Watchdog Timeout
+	reti 			; jmp WDT		; Watchdog Timeout
 	reti 			; jmp TIM2_COMPA	; Timer2 CompareA
 	reti 			; jmp TIM2_COMPB	; Timer2 CompareB
 	reti 			; jmp TIM2_OVF		; Timer2 Overflow
@@ -50,18 +50,18 @@
 	reti 			; jmp USART_RXC		; USART RX Complete
 	reti 			; jmp USART_UDRE	; USART UDR Empty
 	reti 			; jmp USART_TXC		; USART TX Complete
-	reti 			; jmp ADC			; ADC Conversion Complete
+	reti 			; jmp ADC		; ADC Conversion Complete
 	reti 			; jmp EE_RDY		; EEPROM Ready
 	reti 			; jmp ANA_COMP		; Analog Comparator
-	reti 			; jmp TWI			; 2-wire Serial
+	reti 			; jmp TWI		; 2-wire Serial
 	reti 			; jmp SPM_RDY		; SPM Ready
 
 
 ;---------------------------------------------------------------------------
 
 RESET:	ldi r16,high(RAMEND)		; Main program start here after reset
-		out SPH,r16					; Set Stack Pointer to top of RAM
+		out SPH,r16		; Set Stack Pointer to top of RAM
 		ldi r16,low(RAMEND)
 		out SPL,r16
-		sei							; Enable interrupts
+		sei			; Enable interrupts
 
