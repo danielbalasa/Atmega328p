d6f24496bac1dca4b838fe7a2ab4d94684f4bc3a
danielbalasa <daniel.balasa@gmail.com>
2020-05-02 03:35:25 +0200

    Update library_spi.asm

--- library_spi.asm
+++ library_spi.asm
@@ -13,15 +13,23 @@
 
 
 ; -----------------------------------------------------------------------------------------	
-_SPIInit:					; intiliaze the SPI interface
-		sbi DDRB, DDB2			; SlaveSelect or ChipSelect or LOAD
-		sbi DDRB, DDB5			; SCK - clock
-		sbi DDRB, DDB3			; MOSI - the data OUT from master
-		sbi PORTB, PORTB5		; make SS up
+_SPIInit:	; intiliaze the SPI interface
+		; setting the direction of the port pins
+		; DDRB is the Direction Register for Port B
+		sbi DDRB, DDB2			; SlaveSelect or ChipSelect or LOAD (OUTPUT)
+		sbi DDRB, DDB5			; SCK - clock (OUTPUT)
+		sbi DDRB, DDB3			; MOSI - the data OUT from master (OUTPUT)
+		
+		; making the pin 5 on port B (Chip Select) UP - none selected
+		; PORTB is the data register for Port B
+		sbi PORTB, PORTB5		; make CS up - put a HIGH logic (5V) on pin5/portB
 
 
-		ldi r16, 0b0101_0010		;
-		out SPCR, r16
+		; load the SPI settings on the r16
+		; these settings are device dependent
+		; CHANGE THE SETTINGS AS NEEDED BASED ON THE DEVICE YOU ARE CONNECTING WITH
+		ldi r16, 0b0101_0010		
+		out SPCR, r16			; write the settings to the SPI Control Register (SPCR)
 		ret
     
 ;-------------------------------------------------------------------------------------------
