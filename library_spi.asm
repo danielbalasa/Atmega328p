713e1c050a5d3f8d781c358498bc8e8ca7f163f8
danielbalasa <daniel.balasa@gmail.com>
2020-05-02 04:02:58 +0200

    Update library_spi.asm

--- library_spi.asm
+++ library_spi.asm
@@ -13,36 +13,39 @@
 
 
 ; -----------------------------------------------------------------------------------------	
-_SPIInit:	; intiliaze the SPI interface
-		; setting the direction of the port pins
-		; DDRB is the Direction Register for Port B
-		sbi DDRB, DDB2			; SlaveSelect or ChipSelect or LOAD (OUTPUT)
-		sbi DDRB, DDB5			; SCK - clock (OUTPUT)
-		sbi DDRB, DDB3			; MOSI - the data OUT from master (OUTPUT)
+_SPI_MasterInit:	; intiliaze the SPI interface
+			; setting the direction of the port pins
+			; DDRB is the Direction Register for Port B
+			sbi DDRB, DDB2			; SlaveSelect or ChipSelect or LOAD (OUTPUT)
+			sbi DDRB, DDB5			; SCK - clock (OUTPUT)
+			sbi DDRB, DDB3			; MOSI - the data OUT from master (OUTPUT)
 		
-		; making the pin 5 on port B (Chip Select) UP - none selected
-		; PORTB is the data register for Port B
-		sbi PORTB, PORTB5		; make CS up - put a HIGH logic (5V) on pin5/portB
+			; making the pin 5 on port B (Chip Select) UP - none selected
+			; PORTB is the data register for Port B
+			sbi PORTB, PORTB5		; make CS up - put a HIGH logic (5V) on pin5/portB
 
 
-		; load the SPI settings on the r16
-		; these settings are device dependent
-		; CHANGE THE SETTINGS AS NEEDED BASED ON THE DEVICE YOU ARE CONNECTING WITH
-		ldi r16, 0b0101_0010	
-		; the structure of the SPI COntrol Register (SPCR)
-		; SPIE 	= SPI Interrupt Enable		- 0
-		; SPE	= SPI Enable			- 1
-		; DORD	= Data Order			- 0
-		; MSTR	= Master/Slave Select		- 1
-		; CPOL	= Clock Polarity		- 0
-		; CPHA	= Clock Phase			- 0
-		; SPR1	= SPI Clock Rate Select1	- 1
-		; SPR0	= SPI Clock Rate Select0	- 0
-		out SPCR, r16			; write the settings to the SPI Control Register (SPCR)
-		ret
+			; load the SPI settings on the r16
+			; these settings are device dependent
+			; CHANGE THE SETTINGS AS NEEDED BASED ON THE DEVICE YOU ARE CONNECTING WITH
+			
+			ldi r16, 0b0101_0010	
+			; the structure of the SPI COntrol Register (SPCR)
+			; SPIE 	= SPI Interrupt Enable		- 0
+			; SPE	= SPI Enable			- 1
+			; DORD	= Data Order			- 0
+			; MSTR	= Master/Slave Select		- 1
+			; CPOL	= Clock Polarity		- 0
+			; CPHA	= Clock Phase			- 0
+			; SPR1	= SPI Clock Rate Select1	- 1
+			; SPR0	= SPI Clock Rate Select0	- 0
+			
+			; write the settings to the SPI Control Register (SPCR)
+			out SPCR, r16			
+			ret
     
 ;-------------------------------------------------------------------------------------------
-_SPITransmit:					; transmit on SPI the value of register r16
+_SPI_MasterTransmit:				; transmit on SPI the value of register r16
 	
 		out SPDR, r16
 		
